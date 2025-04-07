fd3cf71bae739a0f7782a42f08a4a19b82a2f45a
Pete Carapetyan <pete.carapetyan@gmail.com>
2025-04-07 16:45:15 -0500

    clean up of extra folders

--- functions/src/index.ts
+++
@@ -1,8 +0,0 @@
-import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
-admin.initializeApp(functions.config().firebase);
-
-exports = Object.assign(
-  exports,
-  require("./thumbBanner")
-);
